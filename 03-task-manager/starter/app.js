20c5c5746eef1057a862e5d860524d575557d770
kimberlybayardo <kimberlybayardo@yahoo.com>
2023-01-05 14:57:47 -0800

    Week 6

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,29 +1,23 @@
-// app.get('/api/v1/tasks')- getall the tasks
-// app.post('/api/v1/tasks')-create a new task
-// app.get('/api/v1/tasks/:id')-get single task
-//app.patch('/api/v1/tasks/:id')-update task
-//app.delete('/api/v1/tasks/:id')-delete task
-//CRUD: create, read, update, destroy: the user is able to do this
-
 const express = require('express');
 const app = express();
 const tasks = require('./routes/tasks')
 const connectDB = require('./db/connect')
 require('dotenv').config() //keeps mongo pw safe!!
-// console.log(process.env.MONGO_URI)
-// console.log(process.env.TEST_VALUE)
+const notFound = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
 
 //middleware
+app.use(express.json('./public'))
 app.use(express.json())
 
 //routes
-app.get('/hello', (req,res) => {
-    res.send('Task Manager App')
-})
-
 app.use('/api/v1/tasks', tasks)
 
-const port = 3000
+app.use(notFound)
+app.use(errorHandlerMiddleware)
+
+const port = process.env.PORT || 3000;
+
 
 const start = async () => {
     try {
@@ -36,3 +30,12 @@ const start = async () => {
 
 start();
 
+
+
+
+//app.get('/api/v1/tasks')- getall the tasks
+//app.post('/api/v1/tasks')-create a new task
+//app.get('/api/v1/tasks/:id')-get single task
+//app.patch('/api/v1/tasks/:id')-update task
+//app.delete('/api/v1/tasks/:id')-delete task
+//CRUD: create, read, update, destroy: the user is able to do this
--- 03-task-manager/starter/db/connect.js
+++ 03-task-manager/starter/db/connect.js
@@ -1,9 +1,8 @@
-const mongoose = require('mongoose')
-
 // mongoose
 // .connect(connectionString)
 // .then(() => console.log('CONNECTED TO THE DB'))
 // .catch((err) => console.log(err))
+const mongoose = require('mongoose')
 
 const connectDB = (url) => {
   return mongoose.connect(url, {
@@ -15,3 +14,4 @@ const connectDB = (url) => {
 }
 
 module.exports = connectDB
+
